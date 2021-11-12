67517f8492bb2059138e5e91bf038b39de7f379d
Jorge Luiz <jorgeluizti@hotmail.com>
2021-11-12 07:36:23 -0300

    first commit

---
+++ servidor/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(express.static('../'))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.post('/usuarios', (req, res, next) => {
+    res.send( { 
+        // ...req.body, //  receive data from FrontEnd
+    } )
+    next()
+})
+
+app.get('/usuarios', (req, res) => {
+
+    res.send('<h1>enviando com sucesso</h1>')
+})
+
+app.listen(3000, () => {    
+   console.log('conectado com sucesso...')
+})
